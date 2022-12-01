fc1b6addc512018813df53360ffbf09fce7f01a8
AbianSL <alu0101487458@ull.edu.es>
2022-12-01 18:19:07 +0000

    starting the new day

---
+++ dia_1/src/elf.kt
@@ -0,0 +1,11 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+
+fun ReadFile():List<Int> {
+  var amount_cal: List<Int>
+  Paths.get("home/tara/Escritorio/Personal/Advent_of_code")
+}
+
+fun main() {
+  val 
+}
