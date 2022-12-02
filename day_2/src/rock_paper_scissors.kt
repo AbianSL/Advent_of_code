060f4d0ee9b12a5c64d3f23d480da7864995d9d6
AbianSL <alu0101487458@ull.edu.es>
2022-12-02 09:32:51 +0000

    basic structure

--- day_2/src/rock_paper_scissors.kt
+++ day_2/src/rock_paper_scissors.kt
@@ -4,7 +4,7 @@ import java.io.BufferedReader
 fun ReadFile(): List<String> {
   var amount_cal: MutableList<String> = mutableListOf()
 
-  File("data/advent.txt").useLines { 
+  File("data/example.txt").useLines { 
     lines -> lines.forEach {
       amount_cal.add(it)
     }
@@ -13,9 +13,25 @@ fun ReadFile(): List<String> {
   return amount_cal
 }
 
+fun sum_puntuation(first: String, second: String): Int {
+  
+}
+
 fun main() {
   val calories: List<String> = ReadFile()
+  var first_value: String = "" 
+  var second_value: String = "" 
+
   calories.forEach {
-    println(it)    
+    var fist_second: Boolean = 1
+    for (i in 0..it.length-1) {
+      if (fist_second) {
+        first_value = it[i]
+        first_second = 0
+      } else if (it[i] != '') 
+        second_value = it[i]
+      }
+    }
+    sum_puntuation(first_value, second_value)
   }
 }
