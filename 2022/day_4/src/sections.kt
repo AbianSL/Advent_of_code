9c8c052b35ecf0ed160ac19c673cc7cae6f596a4
AbianSL <alu0101487458@ull.edu.es>
2022-12-04 12:35:23 +0000

    with my example

--- 2022/day_4/src/sections.kt
+++ 2022/day_4/src/sections.kt
@@ -2,7 +2,7 @@ import java.io.File
 import java.io.BufferedReader
 
 fun ReadFile(): List<String> {
-  val items: List<String> = File("data/example.txt").readLines()
+  val items: List<String> = File("data/advent.txt").readLines()
   return items
 }
 
