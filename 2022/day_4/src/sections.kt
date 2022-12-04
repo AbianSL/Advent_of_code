51f356213fab1ba4ed8e6c667a740575d4a34cd1
AbianSL <alu0101487458@ull.edu.es>
2022-12-04 10:53:50 +0000

    all compile correctly

--- 2022/day_4/src/sections.kt
+++ 2022/day_4/src/sections.kt
@@ -7,11 +7,31 @@ fun ReadFile(): List<String> {
 }
 
 fun SeparateInPairs(line: String): Pair<Pair<Int, Int>, Pair<Int, Int>> {
-  var first_value = 0
-  var second_value = 0
+  var first_value: Int = -1000
+  var second_value: Int = -1000
+  var first_second: Boolean = true 
+  var first_pair = Pair(-100, -100) // to know if it doens't do
+  var second_pair = Pair(-100, -100) // to know if it doens't do
+  var first_second_pair: Boolean = true
   for (i in line) {
-
+    if (i >= '0' && i <= '9') {
+      if (first_second) {
+        first_value = i.toInt()
+        first_second = false
+      } else {
+        second_value = i.toInt()
+        first_second = true
+      }
+    } else if (i == ',' || i == line[line.length - 1]) {
+      if (first_second_pair) {
+        first_pair = Pair(first_value, second_value)
+        first_second_pair = false
+      } else {
+        second_pair = Pair(first_value, second_value)
+      }
+    }
   }
+  return Pair(first_pair, second_pair)
 }
 
 fun main() {
