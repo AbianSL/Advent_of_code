8dba4fe7876b3ff95a35b67c27f3ae6758afeed8
AbianSL <alu0101487458@ull.edu.es>
2022-12-05 20:07:24 +0000

    added to not continue

--- 2022/day_5/src/Tracks.kt
+++ 2022/day_5/src/Tracks.kt
@@ -6,13 +6,19 @@ fun ReadFile(name: String): List<String> {
   return items
 }
 
-fun Structure(all_lines: List<String>): List<MutableList<String>> {
+fun Structure(all_lines: List<String>, amount: Int): List<MutableList<String>> {
   var all_sets: MutableList<MutableList<String>> = mutableListOf()
   var single_set: MutableList<String> = mutableListOf()
   var letter: String = ""
   var counter: Int = 0
   var position: Int = 0
   for (i in 0..all_lines.size - 1) {
+    
+    val number = all_lines[i].toList().filter{ it <= '9' && it >= '0' }
+    if (number.size >= 1) {
+      break
+    }
+
     for (j in all_lines[i]) {
       if (j == ' ') {
         if (counter >= 3) {
@@ -29,6 +35,8 @@ fun Structure(all_lines: List<String>): List<MutableList<String>> {
       }
   
       if (j == ']') {
+        println(position)
+        println(letter)
         single_set.add(position, letter)
         letter = ""
         counter = 0
@@ -41,5 +49,5 @@ fun Structure(all_lines: List<String>): List<MutableList<String>> {
 
 fun main(args: Array<String>) {
   val AllLines: List<String> = ReadFile(args[0])
-  val All_tracks: List<MutableList<String>> = Structure(AllLines)
+  val All_tracks: List<MutableList<String>> = Structure(AllLines, args[1])
 }
