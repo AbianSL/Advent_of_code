da6d32bd313f17025de80a9316167a778209bbb7
AbianSL <alu0101487458@ull.edu.es>
2022-12-05 19:19:05 +0000

    basic structure

--- 2022/day_5/src/sections.kt
+++ 2022/day_5/src/sections.kt
@@ -2,25 +2,27 @@ import java.io.File
 import java.io.BufferedReader
 
 fun ReadFile(): List<String> {
-  val items: List<String> = File("data/advent.txt").readLines()
+  val items: List<String> = File("data/example.txt").readLines()
   return items
 }
 
 fun Structure(all_lines: List<String>): List<MutableList<String>> {
   var all_sets: MutableList<MutableList<String>> = mutableListOf()
   var letter: String = ""
-  for (i in all_lines) {
-    if (i == ' ') {
-      continue
-    }
-
-    if (i >= 'A' && i <= 'Z') {
-      letter += i
-    }
-
-    if (i == ']') {
-      all_sets.add(0, i)
-      letter = ""
+  for (i in 0..all_lines.size - 1) {
+    for (j in all_lines[i]) {
+      if (j == ' ') {
+        continue
+      }
+  
+      if (j >= 'A' && j <= 'Z') {
+        letter += j
+      }
+  
+      if (j == ']') {
+        all_sets.add(0, letter)
+        letter = ""
+      }
     }
   }
 }
