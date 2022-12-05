155d769109c917b450b6570433f4f26b8f92592b
AbianSL <alu0101487458@ull.edu.es>
2022-12-05 05:12:03 +0000

    ne  function

--- 2022/day_5/src/sections.kt
+++ 2022/day_5/src/sections.kt
@@ -6,7 +6,25 @@ fun ReadFile(): List<String> {
   return items
 }
 
+fun Structure(all_lines: List<String>): List<MutableList<String>> {
+  var all_sets: MutableList<MutableList<String>> = mutableListOf()
+  var letter: String = ""
+  for (i in all_lines) {
+    if (i == ' ') {
+      continue
+    }
+
+    if (i >= 'A' && i <= 'Z') {
+      letter += i
+    }
+
+    if (i == ']') {
+      all_sets.add(0, i)
+      letter = ""
+    }
+  }
+}
+
 fun main() {
   val All_lines: List<String> = ReadFile()
-
 }
