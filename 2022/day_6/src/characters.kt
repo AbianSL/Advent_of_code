b58eb3309e3fd725681b9139e7841336a5458949
Abian <alu0101487458@ull.edu.es>
2022-12-06 23:30:28 +0000

    Add funciton
    That funciton say if the all the char of a string introduced are different

--- 2022/day_6/src/characters.kt
+++ 2022/day_6/src/characters.kt
@@ -1,6 +1,20 @@
 import java.io.File
 import java.io.BufferedReader
 
+fun AllDifferent(characters: String): Boolean {
+  for (i in 0..characters.lenght) {
+    for (j in 0..characters.lenght) {
+      if (i == j) {
+        continue
+      }
+      if (characters[i] == characters[j]) {
+        return false
+      }
+    }
+  }
+  return true
+}
+
 fun main(args: Array<String>) {
   val AllLines: List<String> = ReadFile(args[0])
   
