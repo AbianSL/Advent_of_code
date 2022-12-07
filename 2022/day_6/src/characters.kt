759f8a4c0f2636902268f6896d885f511427b4ad
Abian <alu0101487458@ull.edu.es>
2022-12-07 00:06:27 +0000

    All done

--- 2022/day_6/src/characters.kt
+++ 2022/day_6/src/characters.kt
@@ -2,8 +2,8 @@ import java.io.File
 import java.io.BufferedReader
 
 fun AllDifferent(characters: String): Boolean {
-  for (i in 0..characters.length) {
-    for (j in 0..characters.length) {
+  for (i in 0..characters.length - 1) {
+    for (j in 0..characters.length - 1) {
       if (i == j) {
         continue
       }
@@ -20,13 +20,14 @@ fun HowManyTakeFour(line: List<String>): Int {
     for (k in 0..i.length - 1) {
       var characters: String = ""
       var amount: Int = 0
-      for (j in 0..4) {
+      for (j in 0..3) {
         if (i.length - k < 4) {
           break
         }
-        characters += line[k + j]
-        amount = k + j
+        characters += i[k + j]
+        amount = k + j + 1
       }
+
       if (AllDifferent(characters)) {
         return amount
       }
